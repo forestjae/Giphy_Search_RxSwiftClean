d2de607f02001fb473775dae64935f928afee310
SJ <forestsj30@gmail.com>
2022-10-26 16:58:19 +0900

    feat: GIF CollectionView Cell 구현

---
+++ Giphy_Search_RxSwift/Giphy_Search_RxSwift/Scene/Search/View/SearchResultContainer/GIFCollectionViewCell.swift
@@ -0,0 +1,81 @@
+//
+//  GIFCollectionViewCell.swift
+//  Giphy_Search_RxSwift
+//
+//  Created by Lee Seung-Jae on 2022/10/25.
+//
+
+import UIKit
+import AVFoundation
+
+final class GIFCollectionViewCell: UICollectionViewCell {
+    private let playerView: PlayerView = {
+        let playerView = PlayerView()
+        return playerView
+    }()
+
+    private var player: AVPlayer?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.clipsToBounds = true
+        self.setupHierarchy()
+        self.setupConstraint()
+        self.contentView.layer.cornerRadius = 10
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.player = nil
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configureContent(_ viewModel: GIFItemViewModel) {
+        self.contentView.backgroundColor = .random()
+        guard let videoURL = URL(string: viewModel.image.gifBundle.gridMp4URL) else {
+            return
+        }
+
+        MediaManager.fetchVideo(videoURL: videoURL) { asset in
+            self.player = AVPlayer(playerItem: AVPlayerItem(asset: asset))
+            DispatchQueue.main.async {
+                self.playerView.player = self.player
+                self.player?.play()
+                self.player?.actionAtItemEnd = .none
+                NotificationCenter.default.addObserver(
+                    forName: .AVPlayerItemDidPlayToEndTime,
+                    object: self.player?.currentItem,
+                    queue: .main
+                ) { [weak self] _ in
+                    self?.player?.seek(to: CMTime.zero)
+                    self?.player?.play()
+                }
+            }
+        }
+    }
+
+    func playVideo() {
+        self.player?.play()
+    }
+
+    func stopVideo() {
+        self.player?.pause()
+    }
+
+    private func setupHierarchy() {
+        self.contentView.addSubview(self.playerView)
+    }
+
+    private func setupConstraint() {
+        self.playerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.playerView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.playerView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            self.playerView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            self.playerView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
+        ])
+    }
+}
